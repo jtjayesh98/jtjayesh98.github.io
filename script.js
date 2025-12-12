e6ad1867c937386521037e725eadfd89c713b43a
Jayesh Tripathi <jt.jayesh98@gmail.com>
2025-12-12 20:59:27 +0530

    Initial site

---
+++ script.js
@@ -0,0 +1,5 @@
+// small niceties
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// If you want to handle form submission via AJAX, add code here.
+// Right now the form posts to Formspree; replace YOUR_FORM_ID in index.html.
